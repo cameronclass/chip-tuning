3c6c00107bea50b8ac3fc7869de0d0f6cf6e6d29
cameronclass <kamrankutumov@gmail.com>
2022-07-11 13:46:21 +0300

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
+new Swiper(".projects__slider", {
+  navigation: {
+    nextEl: ".projects .swiper-button-next",
+    prevEl: ".projects .swiper-button-prev",
+  },
+  pagination: {
+    el: ".projects .swiper-pagination",
+  },
+  loop: true,
+  keyboard: true,
+});
 
+new Swiper(".blog__slider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".blog .swiper-button-next",
+    prevEl: ".blog .swiper-button-prev",
+  },
+  pagination: {
+    el: ".blog .swiper-pagination",
+  },
+  loop: true,
+  keyboard: true,
+});
+
+new Swiper(".reviews__slider", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".reviews .swiper-button-next",
+    prevEl: ".reviews .swiper-button-prev",
+  },
+  pagination: {
+    el: ".reviews .swiper-pagination",
+  },
+  loop: true,
+  keyboard: true,
+});
+
+new Swiper(".videos__slider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".videos .swiper-button-next",
+    prevEl: ".videos .swiper-button-prev",
+  },
+  pagination: {
+    el: ".videos .swiper-pagination",
+  },
+  loop: true,
+  keyboard: true,
+});
